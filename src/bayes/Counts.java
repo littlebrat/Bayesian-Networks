42019a15bfb69b1658c0d3d3989b2137a12aa898
littlebrat <nuno.laurentino.mendes@ist.utl.pt>
2015-05-19 00:26:27 +0100

    Javadoc For Counts class updated

--- src/bayes/Counts.java
+++ src/bayes/Counts.java
@@ -1,8 +1,23 @@
 package bayes;
 
+/**
+ * Class Counts is a specialization of the class TensorToArray that deals with the counting task for each variable of the graph.
+ * 
+ * @author Nuno Mendes
+ * @author Sofia Silva
+ * @author Tiago Ricardo
+ */
+
 public class Counts extends TensorToArray{
 	int me;
 	int[][] countTable;
+	
+	/**
+	 * Constructor method that produces a Count object.
+	 * @param var Index of the variable responsible for this object.
+	 * @param parents Array of Integers with the index' of each parent.
+	 * @param cfgs Object with all the configurations of the network.
+	 */
 	
 	protected Counts(int var,int[] parents,Configurations cfgs){
 		// TODO Auto-generated constructor stub
@@ -18,6 +33,11 @@ public class Counts extends TensorToArray{
 		}
 	}
 	
+	/**
+	 * Calculates the number of total combined configurations that parents of this node take.
+	 * @return Integer value of the total number of combined configurations.
+	 */
+	
 	int getq(){
 		if(myparents.length>0){
 			int res=1;
@@ -29,9 +49,20 @@ public class Counts extends TensorToArray{
 		else return 0;
 	}
 	
+	/**
+	 * Gets the i'th 'B' value used when calculating the MDL score.
+	 * @return double number with B score for this variable.
+	 */
+	
 	double getMyB(){
 		return (cfgs.ri(me)-1)*getq();
 	}
+	
+	/**
+	 * Adds one count to the object, corresponding to entry with k for its configuration and p for the configuration of its parents.
+	 * @param k Integer for its configuration.
+	 * @param p Array with the configuration values of its parents.
+	 */
 	
 	void addcount(int k,int[] p){
 		if(myparents.length>0){
@@ -42,6 +73,12 @@ public class Counts extends TensorToArray{
 		}
 	}
 	
+	/**
+	 * It calculates the number of times the configuration of parents with 'global' configuration appears on the learning set for any configuration of the present variable.
+	 * @param global Integer global configuration of parents of the variable.
+	 * @return Integer with the number of counts.
+	 */
+	
 	int getNij(int global){
 		int res=0;
 		for (int k = 0; k < cfgs.ri(me); k++) {
@@ -50,11 +87,23 @@ public class Counts extends TensorToArray{
 		return res;
 	}
 	
+	/**
+	 * It calculates the number of times the configuration of parents with 'global' configuration appears on the learning set for a configuration 'k' of the present variable.
+	 * @param global Integer global configuration of parents of the variable.
+	 * @param k Integer for its configuration.
+	 * @return Integer with the number of counts.
+	 */
+	
 	int getNijk(int global,int k){
 		if(myparents.length>0)
 			return countTable[k][global];
 		else return countTable[k][0];
 	}
+	
+	/**
+	 * Returns the total number of configurations of the parents of the variable.
+	 * @return number of different combinations of configurations of the parents.
+	 */
 	
 	int getLengthJ(){
 		return countTable[0].length;
