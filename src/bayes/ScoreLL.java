67c3219dcb39fd5f4ecac84c3d8ccde99d79269a
littlebrat <nuno.laurentino.mendes@ist.utl.pt>
2015-05-19 01:47:46 +0100

    MDLScore and LLScore Javadocs

--- src/bayes/ScoreLL.java
+++ src/bayes/ScoreLL.java
@@ -1,17 +1,38 @@
 package bayes;
-
+/**
+ * ScoreLL is a specialization of the abstract score class.
+ * @author Nuno Mendes
+ * @author Sofia Silva
+ * @author Tiago Ricardo 
+ */
 public class ScoreLL extends Score{
+	
+	/**
+	 * Constructor method that setups the scoreLL.
+	 * @param cfg Configurations of the all the variables of the network.
+	 * @param tolearn Learning data set made with a 2-dimensional array of integers.
+	 */
 
 	protected ScoreLL(Configurations cfg,int[][] tolearn) {
 		super(cfg,tolearn);
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Constructor method that setups the scoreLL with a customized pseudo score.
+	 * @param cfg Configurations of the all the variables of the network.
+	 * @param tolearn Learning data set made with a 2-dimensional array of integers.
+	 * @param pseudo parameter customizable to the user that serves as a way of customizing the estimate calculation.
+	 */
+	
 	protected ScoreLL(Configurations cfg,int[][] tolearn,int pseudo) {
 		super(cfg,tolearn,pseudo);
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public double getScore(BayesGraph grp) {
 		// TODO Auto-generated method stub
--- src/bayes/ScoreMDL.java
+++ src/bayes/ScoreMDL.java
@@ -1,11 +1,31 @@
 package bayes;
 
+/**
+ * ScoreMDL is a specialization of the abstract score class.
+ * @author Nuno Mendes
+ * @author Sofia Silva
+ * @author Tiago Ricardo 
+ */
+
 public class ScoreMDL extends Score{
+	
+	/**
+	 * Constructor method that setups the scoreLL.
+	 * @param cfg Configurations of the all the variables of the network.
+	 * @param tolearn Learning data set made with a 2-dimensional array of integers.
+	 */
 
 	protected ScoreMDL(Configurations cfg,int[][] tolearn) {
 		super(cfg,tolearn);
 		// TODO Auto-generated constructor stub
 	}
+	
+	/**
+	 * Constructor method that setups the scoreLL with a customized pseudo score.
+	 * @param cfg Configurations of the all the variables of the network.
+	 * @param tolearn Learning data set made with a 2-dimensional array of integers.
+	 * @param pseudo parameter customizable to the user that serves as a way of customizing the estimate calculation.
+	 */
 	
 	protected ScoreMDL(Configurations cfg,int[][] tolearn,int pseudo) {
 		super(cfg,tolearn,pseudo);
@@ -19,6 +39,10 @@ public class ScoreMDL extends Score{
 		}
 		return res;
 	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	
 	@Override
 	public double getScore(BayesGraph grp) {
