5b9a0d90afe74f4113da8ce52bf6b72b47ba716c
littlebrat <nuno.laurentino.mendes@ist.utl.pt>
2015-05-19 01:30:26 +0100

    Javadoc for TensorToArray class

--- src/bayes/TensorToArray.java
+++ src/bayes/TensorToArray.java
@@ -1,11 +1,22 @@
 package bayes;
 
 import java.util.List;
+/**
+ * Abstract class that cannot be instantiated that provides two methods for dealing with tensor data.
+ * @author Nuno Mendes
+ * @author Sofia Silva
+ * @author Tiago Ricardo
+ */
 
 public abstract class TensorToArray {
 	Configurations cfgs;
 	int[] myparents;
 	
+	/**
+	 * Receives an array of values and generates a mapping to a linear index.
+	 * @param localcfg Integer combination of values of an array.
+	 * @return Integer linear index.
+	 */
 	public int getGlobal(int[] localcfg){
 		int j=localcfg[0];
 		for (int l = 1; l < myparents.length; l++) {
@@ -14,6 +25,12 @@ public abstract class TensorToArray {
 		return j;
 	}
 	
+	/**
+	 * Static method that receives a linear index and a list of the number of different values an index can have. 
+	 * @param global Integer mapped index.
+	 * @param js list of total possible values for each index.
+	 * @return mapping from linear index to a combination of values.
+	 */
 	public static int[] getLocal(int global,List<Integer> js){
 		int[] localcfg=new int[js.size()];
 		if(js.size()==1){
